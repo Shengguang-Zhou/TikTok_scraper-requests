524df48acebef1e1692bcfc1bc65cc3234b1694c
Shengguang-Zhou <68011170+Shengguang-Zhou@users.noreply.github.com>
2023-08-28 00:14:29 +0800

    Update extracting_03.py

--- Scraper/extracting_03.py
+++ Scraper/extracting_03.py
@@ -1,56 +1,73 @@
-import re
+import random
+import requests
+from bs4 import BeautifulSoup
+import time
+import streamlit as st
+import pandas as pd
 
-# TODO:
-#  1. Get Twitter/IG/main account
-#  2. 3rd party social media tool
-#  3. Define a email sender
-#  4. Manage related conversation with influencers
-#  4. Chatbot for FAQ
+def get_tiktok_userinfo(username):
+    user_info = {}
+    userNotFoundMessage = "User ID已改变"
+    #target website
+    url_tiktok = 'https://www.tiktok.com'   # -> check on inspector from chrome
+    combined_url = url_tiktok+'/@'+username
+    # response get
+    response = requests.get(combined_url)
+    if response.status_code == requests.codes.ok:
+        html= response.content
+        # bs4
+        soup = BeautifulSoup(html, 'html.parser')
+        user_subtitle = soup.find('h2',class_='tiktok-1d3qdok-H2ShareSubTitle ekmpd5l7')
+        if user_subtitle:
+            user_info['user_subtitle'] = user_subtitle.text.strip()
+        followers_num = soup.find('strong',attrs={'data-e2e': 'followers-count'})
+        if followers_num:
+            user_info['followers_num'] = followers_num.text.strip()
+        user_bio = soup.find('h2',class_="tiktok-vdfu13-H2ShareDesc e1457k4r3")
+        if user_bio:
+            user_info['user_bio'] = user_bio.text.strip()
+    elif response.status_code == 404:
+        user_info = {"user_subtitle": userNotFoundMessage,
+                     "followers_num": userNotFoundMessage,
+                     "user_bio": userNotFoundMessage}
+    else:
+        raise ValueError('访问被拒绝或其他错误',response.status_code)
+    return user_info
 
-# Extracting email
-def extract_email(text):
-    if not isinstance(text, str):
-        text = str(text)
-    # Regular expression pattern to match email addresses
-    email_pattern = r"[a-zA-Z0-9._%!#$%^&*()\+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-    # Find all matches in the text
-    emails = re.findall(email_pattern, text)
-    if not emails:
-        return '暂无联系方式'
-    # try:
-    #     return ',\n'.join(emails)
-    # except:
-    return ', '.join(emails)
+@st.cache_data()
+def getUsersInfoThru(userlist):
+    info = []
+    placeholder = st.empty()
+    for i in range(len(userlist)):
+        try:
+            user_info = get_tiktok_userinfo(userlist[i])
+            info.append(user_info)
+            placeholder.table(pd.DataFrame(info))
+
+        except Exception as e:
+            print(f"Error for user {i}: {e}")
+        # sleep for avoiding IP ban
+        random_sleep = 1 + 3 * random.random()
+        time.sleep(random_sleep)
+
+    placeholder.empty()
+    return info
 
 
-def extract_websites(text):
-    # Remove email-like patterns from the text
-    email_pattern = r"[a-zA-Z0-9._%!#$%^&*()\+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-    text_without_emails = re.sub(email_pattern, '', text)
-    # Further refined regex pattern to try and capture standalone domains without "http://" or "https://" prefix
-    url_pattern = r'(?:(?<=\s)|(?<=^)|(?<=\W))(http(s)?://[\w\-@./]+|(?<![a-zA-Z0-9@])[\w\-]+(?:\.[a-zA-Z]{2,4})(?:(?:\.[a-zA-Z]{2,4})?)(?![a-zA-Z0-9@\.])[\w\-@./]*)'
-    websites = re.findall(url_pattern, text_without_emails)
-    if not websites:
-        return '暂无相关网页'
-    # Post-process to remove trailing periods from domain names or URLs
-    return ',\n'.join([site for tuple_item in websites for site in tuple_item if site])
 
+##############################################################################################################################
+##############################################################################################################################
+######################################################  TEST CASES     #######################################################
+##############################################################################################################################
+##############################################################################################################################
 
-# Extract phone number -US Phone number
-def extract_phone_numbers(text):
-    # Regular expression pattern to match phone numbers
-    phone_pattern = r'(\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4})'
-    # Find all matches in the text
-    phone_numbers = re.findall(phone_pattern, text)
-    return phone_numbers
-
-
-# Extracting and combining emails from the entire DataFrame
-def email_from_df(df):
-    all_emails = []
-    for _, row in df.iterrows():
-        cell = row.iloc[4]
-        email = extract_email(cell)
-        if email != '暂无联系方式':
-            all_emails.append(email)
-    return ', '.join(all_emails)
+# info = []
+# username = ['shanleysings','alexanderinvicta','TENFLOW','ShiemB7','big_luke_official','tesi.mae']
+#
+# for i in range(len(username)):
+#     user_info = get_tiktok_userinfo(username[i])
+#     random_sleep = 1 + 4 * random.random()
+#     time.sleep(random_sleep)
+#     info.append(user_info)
+#
+# print(info)
